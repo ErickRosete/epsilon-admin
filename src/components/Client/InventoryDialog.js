ef4a42bf0302acd36863cddbad86af7c5bac55e0
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2019-05-02 13:33:09 -0700

    added client rent list and dates to form

--- src/components/Client/InventoryDialog.js
+++ src/components/Client/InventoryDialog.js
@@ -6,7 +6,8 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button/Button";
 import Spinner from "../Spinner/Spinner"
 import Query from "react-apollo/Query";
-import { GET_CLIENT_INVENTORY } from "./constants";
+import { GET_CLIENT_RENTS } from "./constants";
+import Paper from '@material-ui/core/Paper/Paper';
 
 //styles
 import { styles } from "./constants"
@@ -30,61 +31,68 @@ const InventoryDialog = (props) => {
             aria-labelledby="inventory-dialog"
         >
             <DialogTitle id="inventory-dialog">
-                Visualizar inventario de cliente
+                Visualizar rentas de cliente
         </DialogTitle>
 
-            <DialogContent style={{ minHeight: '50vh' }}>
-                <Query query={GET_CLIENT_INVENTORY} variables={{ id: props.clientId }}>
+            <DialogContent className={classes.dialog}>
+                <Query query={GET_CLIENT_RENTS} variables={{ id: props.clientId }}>
                     {
                         ({ loading, error, data }) => {
                             if (loading)
                                 return <Spinner />
                             if (error) return <p>Error :(</p>
-                            const clientInventory = data.clientInventory;
-                            console.log(clientInventory)
+                            const clientRents = data.clientRents;
+                            console.log(clientRents)
 
-                            if (clientInventory.inventoryProducts.length === 0)
-                                return <h3>El cliente no cuenta con ningun producto en renta</h3>
+                            if (clientRents.length === 0)
+                                return <h3>El cliente no cuenta con ninguna renta</h3>
 
-                            const inventoryProducts = clientInventory.inventoryProducts;
-                            const inventoryAccessories = clientInventory.inventoryAccessories;
                             return (
-                                <React.Fragment>
-                                    <h3>Productos</h3>
-                                    <Table className={classes.table}>
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell align="center">Nombre</TableCell>
-                                                <TableCell align="center">Cantidad</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {inventoryProducts.map(invProd => (
-                                                <TableRow key={invProd.product_id}>
-                                                    <TableCell align="center">{invProd.product.name}</TableCell>
-                                                    <TableCell align="center">{invProd.quantity}</TableCell>
-                                                </TableRow>
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                    <h3>Accesorios</h3>
-                                    <Table className={classes.table}>
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell align="center">Nombre</TableCell>
-                                                <TableCell align="center">Cantidad</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {inventoryAccessories.map(invAcc => (
-                                                <TableRow key={invAcc.accessory._id}>
-                                                    <TableCell align="center">{invAcc.accessory.name}</TableCell>
-                                                    <TableCell align="center">{invAcc.quantity}</TableCell>
-                                                </TableRow>
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                </React.Fragment>
+                                clientRents.map(rent => {
+                                    return (
+                                        <div key={rent._id} style={{ width: '100%' }}>
+                                            <Paper className={classes.paper}>
+                                                <h3 className={classes.center}>Renta de {new Date(rent.startDate).toLocaleDateString()} - {new Date(rent.endDate).toLocaleDateString()}</h3>
+                                                <h4 className={classes.subtitle}>Productos</h4>
+                                                <Table className={classes.table}>
+                                                    <TableHead>
+                                                        <TableRow>
+                                                            <TableCell className={classes.tableCell} align="center">Nombre</TableCell>
+                                                            <TableCell className={classes.tableCell} align="center">Cantidad</TableCell>
+                                                        </TableRow>
+                                                    </TableHead>
+                                                    <TableBody>
+                                                        {rent.rentProducts.map(rentProd => (
+                                                            <TableRow key={rentProd._id}>
+                                                                <TableCell className={classes.tableCell} align="center">{rentProd.product.name}</TableCell>
+                                                                <TableCell className={classes.tableCell} align="center">{rentProd.quantity}</TableCell>
+                                                            </TableRow>
+                                                        ))}
+                                                    </TableBody>
+                                                </Table>
+                                                <h4 className={classes.subtitle}>Accesorios</h4>
+                                                <Table className={classes.table}>
+                                                    <TableHead>
+                                                        <TableRow>
+                                                            <TableCell className={classes.tableCell} align="center">Nombre</TableCell>
+                                                            <TableCell className={classes.tableCell} align="center">Cantidad</TableCell>
+                                                        </TableRow>
+                                                    </TableHead>
+                                                    <TableBody>
+                                                        {rent.rentAccessories.map(rentAcc => (
+                                                            <TableRow key={rentAcc._id}>
+                                                                <TableCell className={classes.tableCell} align="center">{rentAcc.accessory.name}</TableCell>
+                                                                <TableCell className={classes.tableCell} align="center">{rentAcc.quantity}</TableCell>
+                                                            </TableRow>
+                                                        ))}
+                                                    </TableBody>
+                                                </Table>
+                                            </Paper>
+                                        </div>
+
+                                    )
+                                })
+
                             )
                         }
                     }
--- src/components/Client/constants.js
+++ src/components/Client/constants.js
@@ -28,9 +28,58 @@ query ClientInventory($id: ID!) {
 }
 `;
 
+export const GET_CLIENT_RENTS = gql`
+query ClientRents($id: ID!) {
+   clientRents(id: $id) {
+     _id
+     startDate
+     endDate
+     client {
+      _id
+      name
+      company
+    }
+    rentProducts {
+      _id
+      code
+      quantity
+      product {
+        _id
+        name
+        imageLinks
+        codes
+      }
+    }
+    rentAccessories {
+      _id
+      quantity
+      accessory {
+        _id
+        name
+      }
+    }
+  }
+}
+`;
+
 //CSS
 export const styles = theme => ({
-    table: {
-        minWidth: 700,
-    },
+  // table: {
+  //   minWidth: 700,
+  // },
+  center: {
+    textAlign: 'center'
+  },
+  dialog: {
+    minHeight: '50vh',
+  },
+  paper: {
+    overflowX: 'auto', marginRight: 'auto', marginLeft: 'auto', marginTop: '15px', padding: '20px', margin: '10px'
+  },
+  subtitle: {
+    textAlign: 'center', marginBottom: '1px'
+  },
+  tableCell: {
+    padding: 0
+  }
 });
